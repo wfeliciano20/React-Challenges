864c1ce338d5e5df6a2536ed69f0da682e91ebe1
william a feliciano rios <william.feliciano2@gmail.com>
2022-03-13 11:58:11 -0400

    moved completed code to a new branch

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,40 +1,11 @@
-import {useState} from 'react';
-
 
 
 function App() {
 
-  const createRandomRGBValue = () => {
-  return Math.floor(Math.random() * 256);
-  };
-
-  const [red, setRed] = useState(createRandomRGBValue());
-  const [green, setGreen] = useState(createRandomRGBValue());
-  const [blue, setBlue] = useState(createRandomRGBValue());
-
-  const handleClick = () => {
-    setRed(createRandomRGBValue());
-    setGreen(createRandomRGBValue());
-    setBlue(createRandomRGBValue());
-  };
 
   return (
-    <div style={{textAlign: 'center'}}>
-      <h1>Click me for color change</h1>
-      <div 
-        style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                margin: '100px auto',
-                height: '300px',
-                width: '300px',
-                backgroundColor: `rgb(${red}, ${green}, ${blue})`
-                }}
-        onClick={handleClick}
-      >
-    <h2>{`rgba(${red}, ${green}, ${blue})`}</h2>
-      </div>
+    <div>
+
     </div>
   );
 }
