9eeaac34af321ec75abfed2aafd10b1a331ffdac
william a feliciano rios <william.feliciano2@gmail.com>
2022-03-12 20:01:02 -0500

    color picker files

---
+++ color-picker/src/App.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+  const createRandomRGBValue = () => {
+  return Math.floor(Math.random() * 256 + 1);
+  };
+
+  const [red, setRed] = useState(createRandomRGBValue());
+  const [green, setGreen] = useState(createRandomRGBValue());
+  const [blue, setBlue] = useState(createRandomRGBValue());
+
+  const handleClick = () => {
+    setRed(createRandomRGBValue());
+    setGreen(createRandomRGBValue());
+    setBlue(createRandomRGBValue());
+  };
+
+  return (
+    <div className="App">
+      <h1>Click me for color change</h1>
+      <div 
+        style={{  
+                margin: '100px auto',
+                height: '300px',
+                width: '300px',
+                backgroundColor: `rgb(${red}, ${green}, ${blue})`
+                }}
+        onClick={handleClick}
+      >
+    <h2>{`rgba(${red}, ${green}, ${blue})`}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
